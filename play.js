1e9dee98f476341191f8a75aa757ecb3b673188a
Andrew Kim <wk@ualberta.ca>
2019-07-31 16:13:13 +0000

    added movement to the snake

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const connect = require('./client');
+const setupInput = require('./input.js');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  stdin.on('data', key => {
-    handleUserInput(key);
-  });
-
-  return stdin;
-
-};
-
-const handleUserInput = function(key) {
-  if (key === `\u0003`) {
-    process.exit();
-  }
-};
 
 console.log('Connecting ...');
-connect();
-setupInput();
+
+setupInput(connect());
